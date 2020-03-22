358ec7e5210dbce3403b122f05b911c763837df3
takuma.sawada <xsjamm4165@yahoo.co.jp>
2020-03-22 05:33:14 +0000

    kadai login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,34 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    name = params[:session][:name].downcase
+    password = params[:session][:password]
+    if login(name,password)
+      flash[:success] = 'ログインに成功しました。'
+      redirect_to root_url
+    else
+      flash.now[:danger] = 'ログインに失敗しました。'
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = 'ログアウトしました。'
+    redirect_to root_url
+  end
+  
+  private
+  
+  def login(name,password)
+    @user = User.find_by(name: name)
+    if @user && @user.authenticate(password)
+      session[:user_id] = @user.id
+      return true
+    else
+      return false
+    end
+  end
+end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,38 +1,40 @@
 class TasksController < ApplicationController
+    before_action :require_user_logged_in
+    before_action :correct_user, only: [:show, :destroy, :update, :edit]
+    
     def index
-        @tasks = Task.all
+        @tasks = current_user.tasks.order(id: :desc)
     end
     
     def show
-        @task = Task.find(params[:id])
     end
     
     def new
-        @task = Task.new
+        @task = current_user.tasks.build
     end
     
     def create
-        @task = Task.new(task_params)
+        @task = current_user.tasks.build(task_params)
         
         if @task.save
             flash[:success] = 'タスクが正常に作成されました'
-            redirect_to @task
+            redirect_to root_url
         else
+            @task = current_user.tasks.order(id: :desc)
             flash.now[:danger] = 'タスクを作成できませんでした'
             render :new
         end
     end
     
     def edit
-        @task = Task.find(params[:id])
+        @task = current_user.tasks.find_by(id: params[:id])
     end
     
     def update
-        @task = Task.find(params[:id])
-        
+       
         if @task.update(task_params)
             flash[:success] = 'タスクは正常に変更されました'
-            redirect_to @task
+            redirect_to root_url
         else
             flash.now[:danger] = 'タスクを更新できませんでした'
             render :edit
@@ -40,17 +42,23 @@ class TasksController < ApplicationController
     end
     
     def destroy
-        @task = Task.find(params[:id])
         @task.destroy
         
         flash[:success] = 'タスクを正常に削除できました'
-        redirect_to tasks_url
+        redirect_back(fallback_location: root_path)
     end
     
     private
 
     def task_params
-    params.require(:task).permit(:content, :status)
+        params.require(:task).permit(:content, :status)
+    end
+    
+    def correct_user
+        @task = current_user.tasks.find_by(id: params[:id])
+        unless @task
+            redirect_to root_url
+        end
     end
 end
 
